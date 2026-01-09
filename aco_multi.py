60a753b1c6f40fef31e6d58791d42c468d478e24
fffakhriah <s23b0128@siswa.umk.edu.my>
2026-01-10 02:45:17 +0800

    Refactor ACO_Multi initialization and fitness methods

--- aco_multi.py
+++ aco_multi.py
@@ -1,58 +1,43 @@
 import numpy as np
 
 class ACO_Multi:
-    def __init__(
-        self, data, ants=20, iterations=50,
-        w1=0.6, w2=0.3, w3=0.1, evap=0.5
-    ):
+    def __init__(self, data, ants=20, iterations=50,
+                 w1=0.6, w2=0.3, w3=0.1, evap=0.5):
+
         self.price = data['ticket_price'].values
         self.demand = data['number_of_persons'].values
         self.pheromone = np.ones(len(self.price))
 
         self.ants = ants
         self.iterations = iterations
+        self.w1 = w1
+        self.w2 = w2
+        self.w3 = w3
         self.evap = evap
-
-        # Weights for multi-objective fitness
-        self.w1 = w1  # revenue importance
-        self.w2 = w2  # demand importance
-        self.w3 = w3  # price penalty
 
     def fitness(self, i):
         revenue = self.price[i] * self.demand[i]
-
-        # MULTI-OBJECTIVE FITNESS (BALANCED)
-        return (
-            self.w1 * revenue +
-            self.w2 * self.demand[i] -
-            self.w3 * self.price[i]
-        )
+        return (self.w1 * revenue +
+                self.w2 * self.demand[i] -
+                self.w3 * self.price[i])
 
     def run(self):
-        best_price, best_fitness = None, -np.inf
+        best_idx = 0
+        best_score = -1e9
         history = []
 
         for _ in range(self.iterations):
-            selected, fitness_vals = [], []
+            probs = self.pheromone / self.pheromone.sum()
+            choices = np.random.choice(len(self.price), self.ants, p=probs)
 
-            prob = self.pheromone / self.pheromone.sum()
-
-            for _ in range(self.ants):
-                i = np.random.choice(len(self.price), p=prob)
-                f = self.fitness(i)
-
-                selected.append(i)
-                fitness_vals.append(f)
-
-                if f > best_fitness:
-                    best_fitness = f
-                    best_price = self.price[i]
+            for idx in choices:
+                score = self.fitness(idx)
+                if score > best_score:
+                    best_score = score
+                    best_idx = idx
 
             self.pheromone *= (1 - self.evap)
+            self.pheromone[best_idx] += abs(best_score) / 1000
+            history.append(best_score)
 
-            for i, f in zip(selected, fitness_vals):
-                self.pheromone[i] += f / 1000
-
-            history.append(best_fitness)
-
-        return best_price, best_fitness, history
+        return self.price[best_idx], best_score, history
